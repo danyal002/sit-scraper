7796c67fa25aa876707ed6dd2db41c78e18ed229
danyal002 <danyal.ak002@gmail.com>
2023-06-25 23:05:31 -0600

    Added discord bot functionality

--- scraper.py
+++ scraper.py
@@ -1,16 +1,16 @@
-import requests
-from bs4 import BeautifulSoup
 import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+import discord
+from dotenv import load_dotenv
 
 def check_website():
     options = Options()
     options.add_argument("--headless")
-    # service = Service('/usr/local/bin/chromedriver')  
-    service = Service('/usr/lib/chromium-browser/chromedriver')  
+    service = Service('/usr/local/bin/chromedriver')  
+    # service = Service('/usr/lib/chromium-browser/chromedriver')  
     driver = webdriver.Chrome(service=service, options=options)
     
     url = 'https://bolig.sit.no/en/' 
@@ -20,19 +20,26 @@ def check_website():
     label = driver.find_element(By.CSS_SELECTOR, 'label[for="Trondheim"].checkbox__StyledLabel-sc-1371sdl-2.gkAgbU')
     
     if not label:
-        # notifyMacOS("Apartment Found", "Book your fucking room right now!!")
-        print("Apartment Found")
+        notifyDiscordBot("An apartment has been found. Go book it!!")
     
     driver.quit()
-
 
 def notifyMacOS(title, text):
     os.system("""
               osascript -e 'display notification "{}" with title "{}"'
               """.format(text, title))
 
-# def notifyiPhone():
-#     o.MxBHak8iaYPi7QllzgUPabZMlnJmTYkd
-
+def notifyDiscordBot(text):
+    load_dotenv()
+    token = os.getenv("DISCORD_TOKEN")
+    
+    bot = discord.Client(intents=discord.Intents.default())
+    
+    @bot.event
+    async def on_ready():
+        await bot.guilds[0].text_channels[0].send(text)
+        await bot.close()
+    
+    bot.run(token)
 
 check_website()
